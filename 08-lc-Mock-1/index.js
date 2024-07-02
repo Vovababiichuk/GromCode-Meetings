a70fc1ed34d46b4710df5c2bd40877d2e8b79d0b
Volodymyr <babihuk888@gmail.com>
2024-07-02 12:42:18 +0300

    lc-8-mock

---
+++ 08-lc-Mock-1/index.js
@@ -0,0 +1,199 @@
+// ====First Task==========
+
+// v1
+// const getInterval = numbers => {
+// 	return [Math.min(...numbers), Math.max(...numbers)]
+// }
+
+// // examples
+// console.log(getInterval([1, 50, 0, 90, 5])); // [0, 90]
+// console.log(getInterval([-10, -2, -5, -20, -3])); // [-20, -2]
+// console.log(getInterval([100, 200, 300, 400, -500])); // [-500, 400]
+
+// v2
+// const getInterval = numbers => {
+// 	return numbers.reduce((acc, num) => [
+//     Math.min(acc[0], num),
+//     Math.max(acc[1], num)
+//   ], [Infinity, -Infinity]);
+// }
+
+// console.log(getInterval([1, 50, 0, 90, 5])); // [0, 90]
+// console.log(getInterval([-10, -2, -5, -20, -3])); // [-20, -2]
+// console.log(getInterval([100, 200, 300, 400, -500])); // [-500, 400]
+
+// v3
+
+// const getInterval = numbers => {
+// 	return numbers.reduce((acc, num) => {
+// 		let min = acc[0];
+// 		let max = acc[1];
+
+// 		if (num < min) min = num;
+// 		if (num > max) max = num;
+
+// 		return [min, max];
+// 	}, [numbers[0], numbers[0]]);
+// }
+
+// console.log(getInterval([1, 50, 0, 90, 5])); // [0, 90]
+// console.log(getInterval([-10, -2, -5, -20, -3])); // [-20, -2]
+// console.log(getInterval([100, 200, 300, 400, -500])); // [-500, 400]
+
+
+// TASK 3
+
+// v1
+
+// function intersection(firstArr, secondArr) {
+//   // put your code here
+// 	return firstArr.reduce((acc, num) => {
+// 		if (secondArr.includes(num)) {
+// 			acc.push(num)
+// 		}
+// 		return acc;
+// 	}, [])
+// }
+
+// // examples
+// console.log(intersection([1, 2, 3], [2, 3, 4, 5])); // returns ==> [2, 3]
+// console.log(intersection([1, 2, 0], [2, 3, -1, 5])); // returns ==> [2]
+// console.log(intersection([4, 8, -1], [1, 2])); // returns ==> []
+
+	// v2
+
+	// function intersection(firstArr, secondArr) {
+	// 	// put your code here
+	// 	return firstArr.filter(el => secondArr.includes(el))
+	// }
+	
+	// // examples
+	// console.log(intersection([1, 2, 3], [2, 3, 4, 5])); // returns ==> [2, 3]
+	// console.log(intersection([1, 2, 0], [2, 3, -1, 5])); // returns ==> [2]
+	// console.log(intersection([4, 8, -1], [1, 2])); // returns ==> []
+	
+
+// ===========Task 4 ===================
+
+// // input: name
+// // output: list book [{}]
+
+// // algo
+
+// // 1. iteration arr
+// // 2. if count > 0 output obj
+// // 3. output with obj title
+
+// const booksStorage = [
+// 	{
+// 		author: 'Bill Gates',
+// 		title: 'The Road Ahead',
+// 		booksCount: 5,
+// 		bookingsCount: 10,
+// 	},
+// 	{
+// 		author: 'Bill Gates',
+// 		title: 'Slow Horses',
+// 		booksCount: 12,
+// 		bookingsCount: 21,
+// 	},
+// 	{
+// 		author: 'Steve Jobs',
+// 		title: 'Walter Isaacson',
+// 		booksCount: 0,
+// 		bookingsCount: 5,
+// 	},
+// 	{
+// 		author: 'Suzanne Collins',
+// 		title: 'Mockingjay: The Final Book of The Hunger Games',
+// 		booksCount: 2,
+// 		bookingsCount: 7,
+// 	},
+// 	{
+// 		author: 'Taras Shevchenko',
+// 		title: 'Kobzar',
+// 		booksCount: 0,
+// 		bookingsCount: 10,
+// 	},
+// ]
+
+// /**
+//  * @param {string} author
+//  * @return {object[]}
+//  */
+// function getAvailableBooks(author) {
+// 	return booksStorage.filter(el => el.booksCount > 0).map(el => {
+// 		const newObj = {};
+// 		newObj.author = el.author;
+// 		newObj.title = el.title;
+// 		return newObj
+// 	})
+
+// }
+
+// // examples
+// // #1
+// getAvailableBooks()
+// // ===> returns
+// // [
+// //   {
+// //     author: 'Bill Gates',
+// //     title: 'The Road Ahead',
+// //   },
+// //   {
+// //     author: 'Bill Gates',
+// //     title: 'Slow Horses',
+// //   },
+// //   {
+// //     author: 'Suzanne Collins',
+// //     title: 'Mockingjay: The Final Book of The Hunger Games',
+// //   },
+// // ];
+
+// // #2
+// getAvailableBooks('Taras Shevchenko')
+// // ===> returns
+// // [
+// //   {
+// //     author: 'Taras Shevchenko',
+// //     title: 'Kobzar',
+// //   },
+// // ];
+
+
+// ===================Task 5===========================
+
+// function getTopStudent(studentsData) {
+//   const topStudent = studentsData.reduce((acc, curr) =>
+//     (acc.points > curr.points) ? acc : curr
+//   );
+
+//   return topStudent;
+// }
+
+//   // return topStudent;
+
+// const students = [
+//   { id: 1, firstName: 'Ivan', lastName: 'Ivanov', points: 95 },
+//   { id: 2, firstName: 'Petro', lastName: 'Petrov', points: 85 },
+//   { id: 3, firstName: 'Anna', lastName: 'Annikova', points: 98 },
+//   { id: 4, firstName: 'Svitlana', lastName: 'Svitlichna', points: 91 },
+//   { id: 5, firstName: 'Mykola', lastName: 'Mykolenko', points: 88 },
+// ];
+
+// console.log(getTopStudent(students));
+
+// const students2 = [
+//   { id: 1, firstName: 'Ivan', lastName: 'Ivanov', points: 9 },
+//   { id: 2, firstName: 'Petro', lastName: 'Petrov', points: -5 },
+//   { id: 3, firstName: 'Mykola', lastName: 'Mykolenko', points: 88 },
+// ];
+
+// console.log(getTopStudent(students2));
+
+// const students3 = [
+//   { id: 1, firstName: 'Ivan', lastName: 'Ivanov', points: 0 },
+//   { id: 2, firstName: 'Mykola', lastName: 'Mykolenko', points: -88 },
+// ];
+
+// console.log(getTopStudent(students3));
--- first-prepare-to mock/index.js
+++ first-prepare-to mock/index.js
@@ -48,7 +48,7 @@
 // console.log(swap([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
 // console.log(swapManual([1, 10, 9, 11])); // ==> [10, 9, 11, 1]
 
-//! ============Arrays Methods (Lesson-17)======================
+//! ============Arrays Methods (Lesson-7)======================
 
 // TODO -- Filter()
 
@@ -169,3 +169,404 @@
 
 // const reverseArray = arr => (Array.isArray(arr) ? arr.slice().reverse() : null);
 // console.log(reverseArray([1, 2, 3, 4, 5]));
+
+//! =============================Object Basics Less-8=====================
+
+// TODO -- Reduce()
+
+// const transformToObject = arr => {
+//   return arr.reduce((acc, curr) => {
+// 		acc[curr] = curr;
+// 		return acc;
+// 	}, {})
+// };
+
+// console.log(transformToObject(['a', 17.1, 'John Doe'])); // ==> { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
+
+// TODO -- Object.Values
+
+// const concatProps = obj => {
+//   return Object.keys(obj);
+// };
+
+// console.log(concatProps({ name: 'John Doe', age: 17, interest: 'football' })); // ==> ['John Doe', 17, 'football']
+
+// TODO -- Read Properties
+
+// const transaction = {
+//   currency: 'USD',
+//   value: 170,
+//   operation: 'sale',
+//   agent: {
+//     country: 'Ukraine',
+//     company: 'NYSE',
+//   },
+//   'operation time': 1584029990001,
+// };
+
+// /* 1. Выведи в консоль значение свойства 'currency' с помощью точки */
+//! console.log(transaction.currency);
+
+// /* 2. Выведи в консоль значение свойства 'value' с помощью квадратных скобок */
+//! console.log(transaction['value']);
+
+// /* 3. Выведи в консоль значение свойства обьекта transaction, название которого находится в переменной key - используй квадратные скобки */
+// const key = 'operation';
+//! console.log(transaction[key]);
+
+// /* 4. Введи в консоль значение свойства 'company' используя квадратные скобки */
+//! console.log(transaction.agent['company']);
+
+// /* 5. Выведи в консоль значение свойства 'country' используя точку */
+//! console.log(transaction.agent.country);
+
+// /* 6. Выведи в консоль значение свойства 'operation time' используя квадратные скобки */
+//! console.log(transaction['operation time']);
+
+// TODO -- Assigning Properties
+
+// const user = {
+//   name: 'Tom',
+//   age: 17,
+// };
+
+// /* присвой объекту свойство hobby со значением 'football' с помощью точки */
+//! user.hobby = 'footbal';
+
+// /* название свойства хранится в переменной anotherKey. Присвой объекту такое свойство со значением false */
+// const anotherKey = 'married';
+//! user[anotherKey] = false;
+
+// /* присвой объекту свойство 'favorite music' со значением 'rock' */
+//! user['favorite music'] = 'rock';
+
+// /* присвой объекту свойство 'address', которое будет пустым объкетом */
+//! user.address = {};
+
+// /* присвой объекту в 'address' свойство 'country', со значением 'Ukraine' */
+
+// !user.address.country = 'Ukraine';
+
+// /* присвой объекту в 'address' свойство 'building', со значением 17 */
+//! user.address.building = 17;
+
+// /* выведи в консоль итоговый объект user */
+// console.log(user);
+
+// TODO -- Iteration Keys
+// function getKeys(obj) {
+//   return Object.keys(obj);
+// }
+
+// console.log(getKeys({ name: 'John Doe', age: 17, interest: 'football' }));
+
+// TODO -- Retrieving Properties
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const pickProps = (obj, props) => {
+// 	return Object.keys(obj).reduce((acc, key) => {
+// 		if (props.includes(key)) {
+// 			acc[key] = obj[key] //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 		}
+// 		return acc
+// 	}, {})
+// }
+
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c'])) // ==> { a: 1, c: 3 }
+// console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex'])) // ==> { a: 1, c: 3 }
+
+// TODO -- Filter Object
+// TODO -- Filter Object
+// TODO -- Filter Object
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const getAdults = obj => {
+// 	const newObj = {}
+// 	for (let key in obj) {
+// 		if (obj[key] >= 18) {
+// 			newObj[key] = obj[key] //!!!!!!!!!!!!!!!!!!!!!!!1
+// 		}
+// 	}
+// 	return newObj;
+// }
+
+// console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })) // ==> { 'John Doe': 19, Bob: 18 }
+// console.log(getAdults({ Ann: 56, Andrey: 7 })) // ==> { Ann: 56 }
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const getAdults = obj => {
+// 	return Object.keys(obj).reduce((acc, key) => {
+// 		console.log(key)
+// 		console.log(obj[key])
+// 		if (obj[key] >= 18) {
+// 			acc[key] = obj[key] //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 		}
+// 		return acc;
+// 	}, {})
+// }
+
+// console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })) // ==> { 'John Doe': 19, Bob: 18 }
+// console.log(getAdults({ Ann: 56, Andrey: 7 })) // ==> { Ann: 56 }
+
+//! ===========================Object(keys, value, entries)================
+
+// TODO -- Entries/Reduce()
+
+// const getAdults = obj => {
+// 	return Object.entries(obj).reduce((acc, [key, value]) => {
+// 		if (value >= 18) {
+// 			acc.push(key)
+// 		}
+// 		return acc;
+// 	}, [])
+// }
+
+// console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 }))
+
+// TODO -- copyObject
+
+// const copyObj = obj => {
+// 	// return Object.assign({}, obj, {test: 'yyy'})
+// 	return {...obj}
+// }
+
+// console.log(copyObj({ 'John Doe': 19, Tom: 17, Bob: 18 }))
+
+// TODO -- Iterate object keys
+
+// function getKeys(obj) {
+// 	return Object.keys(obj).forEach((key => console.log(key)))
+// }
+
+// console.log(getKeys({ name: 'Denis', city: 'Warsaw', englishLevel: 'A1' }))
+// // prints ===>
+// // name
+// // city
+// // englishLevel
+
+// TODO -- Add Properties
+
+// function addPropertyV4(userData, userId) {
+// 	return {...userData, id: userId}
+// }
+
+// const user = {
+// 	name: 'Sam',
+// }
+
+// console.log(addPropertyV4(user, '1234567')) // ==> { name: 'Sam', id: '1234567' }
+
+// TODO -- Merge objects
+
+// function mergeObjectsV1(obj1, obj2) {
+//   return {...obj1, ...obj2};
+// }
+
+// const obj1 = {
+//   name: 'Tom',
+//   age: 17,
+// };
+
+// const obj2 = {
+//   name: 'Bob',
+//   student: false,
+// };
+
+// console.log(mergeObjectsV1(obj1, obj2)); // ==> { name: 'Bob', age: 17, student: false }
+
+// TODO -- Object to Array
+
+// const rooms = {
+// 	room1: [{ name: 'Jack' }, { name: 'Ann' }, { name: 'Tom' }, { name: 'John' }],
+// 	room2: [{ name: 'Pol' }, { name: 'Julia' }, { name: 'Bob' }],
+// 	room3: [{ name: 'Denis' }, { name: 'Laurie' }],
+// 	room4: [{ name: 'Matt' }],
+// }
+
+// const getPeople = obj => {
+// return Object.values(obj).flat().map(room => room.name)
+// }
+
+// console.log(getPeople(rooms))
+
+// TODO -- Count/Reduce()
+
+// const getTotalRevenue = transactions => {
+// 	return transactions.reduce((acc, curr) => acc + curr.amount, 0)
+// }
+
+// const dayTransactions = [
+// 	{ userId: 22, amount: 60, operation: 'sell' },
+// 	{ userId: 22, amount: 160, operation: 'buy' },
+// 	{ userId: 44, amount: 90, operation: 'sell' },
+// ]
+
+// console.log(getTotalRevenue(dayTransactions)) // ==> 310
+
+// TODO -- Add Computed Property
+
+// function addPropertyV1(obj, key, value) {
+// 	return { ...obj, [key]: value }
+// }
+
+// const transaction = {
+// 	value: 170,
+// }
+
+// console.log(addPropertyV1(transaction, 'currency', 'USD')) // ==> { value: 170, currency: 'USD' }
+
+// TODO -- Build Object
+
+// function buildObject(keysList, valuesList) {
+// 	return keysList.reduce((acc, key, index) => {
+// 		acc[key] = valuesList[index];
+// 		return acc;
+// 	}, {})
+// }
+// // examples
+// const keys = ['name', 'address', 'age']
+// const values = ['Vasyl', 'Ukraine', 34]
+// const result = buildObject(keys, values) // ==> { name: 'Vasyl', address: 'Ukraine', age: 34 }
+// console.log(result);
+
+// TODO -- Compare two objects //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// function compareObjects(firstObj, secondObj) {
+// 	const keysFirstObj = Object.keys(firstObj)
+// 	const keysSecondObj = Object.keys(secondObj)
+
+// 	if (keysFirstObj.length !== keysSecondObj.length) {
+// 		return false
+// 	}
+
+// 	return keysFirstObj.every(
+// 		key => keysSecondObj.includes(key) && firstObj[key] === secondObj[key] //!!!!!!!!!!!!!!
+// 	)
+// }
+
+// const obj1 = {
+// 	name: 'Tom',
+// 	age: 17,
+// }
+
+// const obj2 = {
+// 	name: 'Bob',
+// 	age: 17,
+// }
+
+// const obj3 = {
+// 	name: 'Bob',
+// 	age: 17,
+// 	student: false,
+// }
+
+// const obj4 = {
+// 	name: 'Tom',
+// 	age: 17,
+// }
+
+// const obj5 = {
+// 	age: 17,
+// 	name: 'Tom',
+// }
+
+// console.log(compareObjects(obj1, obj2)) // ==> false
+// console.log(compareObjects(obj2, obj3)) // ==> false
+// console.log(compareObjects(obj1, obj4)) // ==> true
+// console.log(compareObjects(obj4, obj5)) // ==> true
+
+// TODO -- Object in Array/Destruction //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const getCustomersList = obj => {
+// 	return Object.entries(obj).map(([id, value]) => ({...value, id})).sort((a, b) => a.age - b.age)
+// }
+
+// const customers = {
+// 	'customer-id-1': {
+// 		name: 'William',
+// 		age: 54,
+// 	},
+// 	'customer-id-2': {
+// 		name: 'Tom',
+// 		age: 17,
+// 	},
+// 	'customer-id-3': {
+// 		name: 'Bob',
+// 		age: 45,
+// 	},
+// }
+
+// console.log(getCustomersList(customers))
+
+//! =======================STRING==============================
+
+// TODO -- Calculator
+
+// v1
+
+// const calc = str => {
+// 	const arr = str.split(' ')
+// 	const [a, operator, b] = arr
+// 	const num1 = Number(a)
+// 	const num2 = Number(b)
+
+// 	let res
+// 	switch (operator) {
+// 		case '+':
+// 			res = num1 + num2
+// 			break
+// 		case '-':
+// 			res = num1 - num2
+// 			break
+// 		case '*':
+// 			res = num1 * num2
+// 			break
+// 		case '/':
+// 			res = num1 / num2
+// 			break
+// 		default:
+// 			return 'Error'
+// 	}
+// 	return `${str} = ${res}`
+// }
+
+// console.log(calc('1 + 2'))
+
+// v2
+
+// const calc = str => {
+// 	const arr = str.split(' ')
+// 	const [a, operator, b] = arr
+// 	const num1 = Number(a)
+// 	const num2 = Number(b)
+
+// const operations = {
+// 	'+': (x, y) => x + y,
+// 	'-': (x, y) => x - y,
+// 	'*': (x, y) => x * y,
+// 	'/': (x, y) => x / y,
+// }
+
+// const res = operations[operator] ? operations[operator](num1, num2) : 'Error'
+// return `${str} = ${res}`
+// }
+
+// console.log(calc('1 + 2'))
+
+// TODO -- Reverse String
+
+// const reverseStr = (str) => {
+// 	return str.split('').reverse().join('')
+// }
+
+// console.log(reverseStr('Hello!'));
+
+// TODO -- Clean Data
+
+// const cleanTransactionsList = (arr) => {
+//   return arr.map(el => +el).filter(el => !isNaN(el)).map(el => `$${el.toFixed(2)}`)
+// }
+
+// console.log(cleanTransactionsList(['  1.9 ', '16.4', 17, ' 1 dollar ']));
